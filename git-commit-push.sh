547f07c5d38ca2def37d0e5d8c001b505919e622
gitwood8 <118898632+gitwood8@users.noreply.github.com>
2023-06-08 12:01:41 +0300

    Update git-commit-push.sh

--- git-commit-push.sh
+++ git-commit-push.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
-
-read -p "Write a comment: " COM
-git add .
-git commit -m "$COM"
-git push
+echo -e "\n Previous commit comment: $(git log -1 --pretty=%B)\n"
+read -p "Enter a comment for commit: " COM && echo ""
+read -p "Ready to Commit and Push with comment: '$COM'? y/n: " ANS && echo ""
+if [ $ANS == "y" ] 
+then
+    git add . #/home/wood/jenkins/jenkins-deploy-app/
+    git commit -m "$COM"
+    git push
+else
+    echo "Aborted" && echo ""
+fi
